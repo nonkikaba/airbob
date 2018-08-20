ded98518fc03a13047bb6cac660cf08a983d268b
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-08-20 14:03:42 +0900

    edit files for facebook authentication
    Facebookでのログインに必要なコードを記述。ほとんどgit hubからのコピペ

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,6 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-  #config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -55,14 +53,15 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address: 'smtp.gmail.com',
-  #   domain: 'gmail.com',
-  #   port: 587,
-  #   enable_starttls_auto: true,
-  #   authentication: 'plain',
-  #   user_name: ENV['GMAIL_ADDRESS'],
-  #   password: ENV['GMAIL_ID']
-  # }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    enable_starttls_auto: true,
+    authentication: 'plain',
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_ID']
+  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  devise_for :users,
+            path: '',
+            path_name: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+            controllers: {omnniauth_callbacks: 'users/omniauth_callbacks'}
 
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  
 end
