79c369ff18580154918d1068e7c3a35246665458
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-08-21 14:14:15 +0900

    edit routes.rb for facebook ahthentication
    rb.3:facebook-omniauthのgithubページからのコピペ
    rb.4-6:pathの名前を変更(/sign_in → /login)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users,
-            path: '',
-            path_name: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
-            controllers: {omnniauth_callbacks: 'users/omniauth_callbacks'}
-
   root 'pages#home'
+  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'},
+            path: '',
+            path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'}
+            
+
+  
   
 end
