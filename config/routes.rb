89e43f8b2cbb91cebaf6da333cffd6758ba508d2
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-08-30 14:14:36 +0900

    edit rooms routing
    rb.10:memberブロックで自分で作成したアクションのルーティングを追加。memeberブロックで作成するパスは、パス内にidパラメータを
    含む。

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,16 @@ Rails.application.routes.draw do
               path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'}
 
   resources :users, only: [:show]
-            
+  resources :rooms, except: [:edit] do
+    member do
+      get 'listing'
+      get 'pricing'
+      get 'description'
+      get 'photo_upload'
+      get 'amenities'
+      get 'location'
+    end
+  end
 
   
   
