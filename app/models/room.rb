a8f3fd45e2878c034d872a5181ac87bb717b3163
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-08-31 14:01:04 +0900

    modify a bit
    validateをかけるパラメータ名が間違っていたので修正

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,7 +3,7 @@ class Room < ApplicationRecord
 
   validates :home_type, presence: true
   validates :room_type, presence: true
-  validates :accommodate_type, presence: true
-  validates :bed_type, presence: true
-  validates :bath_type, presence: true
+  validates :accommodate, presence: true
+  validates :bed_room, presence: true
+  validates :bath_room, presence: true
 end
