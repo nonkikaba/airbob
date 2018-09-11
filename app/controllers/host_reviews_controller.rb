06731d4d1113b95d866727a33f805bb633e6aa5a
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-09-11 13:48:45 +0900

    edit

---
+++ app/controllers/host_reviews_controller.rb
@@ -0,0 +1,28 @@
+HostReviewsController < ApplicationController
+ 
+def create
+
+  @reservation = Reservation.where(
+                    id: host_review_params[:reservation_id],
+                    room_id: host_review_params[:room_id],
+                    user_id: host_review_params[:guest_id]  
+  )
+
+  @host_review = current_user.host_reviews.create(host_review_params)
+  redirect_back(fallback_location: request.referer)
+end
+
+def destroy
+  @host_review = Review.find(params[:id])
+  @host_review.destroy
+
+  redirect_back(fallback_location: request.referer, notice: "Removed")
+end
+
+
+private
+
+    def host_review_params
+      params.require(:host_review).permit(:comment, :star, :room_id, :reservation_id, :guest_id)
+    end
+end
