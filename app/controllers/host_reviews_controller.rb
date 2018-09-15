5544d9825f4e65c651db4aeabb66b89ccc764c5b
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-09-15 22:30:40 +0900

    edit

--- app/controllers/host_reviews_controller.rb
+++ app/controllers/host_reviews_controller.rb
@@ -1,12 +1,30 @@
 HostReviewsController < ApplicationController
  
 def create
+  # step1: check if the reservation exist (room_id, guest_id, host_id)
+
+  # step2: check if the current host already reviewed the guest in this reservation
 
   @reservation = Reservation.where(
                     id: host_review_params[:reservation_id],
                     room_id: host_review_params[:room_id],
                     user_id: host_review_params[:guest_id]  
-  )
+                    )
+
+  if !@reservation.nil?
+    @has_reviewed = HostReview.where(
+                      reservation_id: @reservation.id,
+                      guest_id: host_review_params[:guest_id]
+                      ).first
+
+    if @has_reviewed.nil?
+        #allow to review
+    else
+      # already reviewed
+    end
+  else
+    flash[:alert] = "予約情報が見つかりません"
+  end
 
   @host_review = current_user.host_reviews.create(host_review_params)
   redirect_back(fallback_location: request.referer)
